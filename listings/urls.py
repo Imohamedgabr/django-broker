1ea95e4e251f8cdab1c20079e2102ee54c5b1000
imohamedgabr <imohamedgabr@outlook.com>
2019-11-01 22:57:37 +0200

    added navbar linking with urls

---
+++ listings/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+# bring in the views
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='listings'),
+    path('<int:listing_id>', views.listing, name='listing'),
+    path('search', views.search, name='search')
+]
